cbbe77daba044b235c5de7c03a82052706a13297
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-09-18 07:16:58 +0700

    PROD

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
 'use strict'
 var response = require('./res');
-var connection = require('./conn');
+// var connection = require('./conn');
 
 const express = require('express')
 const users = express.Router()
@@ -15,75 +15,75 @@ users.use(cors())
 
 process.env.SECRET_KEY = '4sri'
 
-exports.users = function(req,res){
-	    connection.query('SELECT * FROM person', function (error, rows, fields){
-        if(error){
-            return res.send({ error: false, data: results, message: 'users list.' });
-        } else{
-            response.ok(rows, res)
-        }
-    });
-};
+// exports.users = function(req,res){
+// 	    connection.query('SELECT * FROM person', function (error, rows, fields){
+//         if(error){
+//             return res.send({ error: false, data: results, message: 'users list.' });
+//         } else{
+//             response.ok(rows, res)
+//         }
+//     });
+// };
 
-exports.index = function(req,res){
-	response.ok("... status", res)
-}
+// exports.index = function(req,res){
+// 	response.ok("... status", res)
+// }
 
-exports.findUsers = function(req, res) {
-    var user_id = req.params.user_id;
-    connection.query('SELECT * FROM person where id = ?',
-    [ user_id ], 
-    function (error, rows, fields){
-        if(error){
-        	return res.send({ error: false, data: results, message: 'users list.' });
-        } else{
-            response.ok(rows, res)
-        }
-    });
-};
+// exports.findUsers = function(req, res) {
+//     var user_id = req.params.user_id;
+//     connection.query('SELECT * FROM person where id = ?',
+//     [ user_id ], 
+//     function (error, rows, fields){
+//         if(error){
+//         	return res.send({ error: false, data: results, message: 'users list.' });
+//         } else{
+//             response.ok(rows, res)
+//         }
+//     });
+// };
 
 
-exports.createUsers = function(req, res) {
-    var first_name = req.body.first_name;
-    var last_name = req.body.last_name;
-    connection.query('INSERT INTO person (first_name, last_name) values (?,?)',
-    [ first_name, last_name ], 
-    function (error, rows, fields){
-        if(error){
-            console.log(error)
-        } else{
-            response.ok("Berhasil menambahkan user!", res)
-        }
-    });
-};
+// exports.createUsers = function(req, res) {
+//     var first_name = req.body.first_name;
+//     var last_name = req.body.last_name;
+//     connection.query('INSERT INTO person (first_name, last_name) values (?,?)',
+//     [ first_name, last_name ], 
+//     function (error, rows, fields){
+//         if(error){
+//             console.log(error)
+//         } else{
+//             response.ok("Berhasil menambahkan user!", res)
+//         }
+//     });
+// };
 
-exports.updateUsers = function(req, res) {
-    var user_id = req.body.user_id;
-    var first_name = req.body.first_name;
-    var last_name = req.body.last_name;
-    connection.query('UPDATE person SET first_name = ?, last_name = ? WHERE id = ?',
-    [ first_name, last_name, user_id ], 
-    function (error, rows, fields){
-        if(error){
-            console.log(error)
-        } else{
-            response.ok("Berhasil merubah user!", res)
-        }
-    });
-};
+// exports.updateUsers = function(req, res) {
+//     var user_id = req.body.user_id;
+//     var first_name = req.body.first_name;
+//     var last_name = req.body.last_name;
+//     connection.query('UPDATE person SET first_name = ?, last_name = ? WHERE id = ?',
+//     [ first_name, last_name, user_id ], 
+//     function (error, rows, fields){
+//         if(error){
+//             console.log(error)
+//         } else{
+//             response.ok("Berhasil merubah user!", res)
+//         }
+//     });
+// };
 
-exports.deleteUsers = function(req, res) {
-    var user_id = req.body.user_id;
-    connection.query('DELETE FROM person WHERE id = ?',
-    [ user_id ], 
-    function (error, rows, fields){
-        if(error){
-            console.log(error)
-        } else{
-            response.ok("Berhasil menghapus user!", res)
-        }
-    });
-};
+// exports.deleteUsers = function(req, res) {
+//     var user_id = req.body.user_id;
+//     connection.query('DELETE FROM person WHERE id = ?',
+//     [ user_id ], 
+//     function (error, rows, fields){
+//         if(error){
+//             console.log(error)
+//         } else{
+//             response.ok("Berhasil menghapus user!", res)
+//         }
+//     });
+// };
 
 exports.authUsers = function(req, res){
   User.findOne({
